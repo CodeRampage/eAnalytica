7f240c1e71e25edc590816e1ee833a1614c68a67
Ngoveni <Ngoveni@DESKTOP-C7ATCFQ>
2017-04-17 22:34:54 +0200

    initial Commit

---
+++ Source/Fasetto.Word/DataModels/ApplicationPage.cs
@@ -0,0 +1,13 @@
+﻿namespace Emissions_Analytica
+{
+    /// <summary>
+    /// A Page of the Application
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// The Data browsing page for the BI System
+        /// </summary>
+        Browser = 0,
+    }
+}
---
+++ Source/Fasetto.Word/ValueConverter/ApplicationPageValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using Emissions_Analytica.Pages;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Emissions_Analytica
+{
+
+    /// <summary>
+    /// Converts the <see cref="ApplicationPage"/> to an actual view/page
+    /// </summary>
+    public class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find appropriate page
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.Browser:
+                    return new WelcomePage();
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
